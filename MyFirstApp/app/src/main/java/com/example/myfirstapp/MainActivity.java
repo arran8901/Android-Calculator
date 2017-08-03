dcd006a9d5fd8d9a5ad8a39d25c25c3ab17a8e86
Arran O'S <30627743+arran8901@users.noreply.github.com>
2017-08-03 10:16:40 +0100

    Better error handling in MainActivity.java

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -23,13 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void button0Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
-        if(!resultBox.getText().toString().equals(""))
+        if(resultBox.getText().length() > 0 && Double.parseDouble(resultBox.getText().toString()) != 0) {
+            setupResultBox();
             resultBox.append("0");
+        }
     }
 
     public void buttonDecPointPressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
         if(!resultBox.getText().toString().contains("."))
+            setupResultBox();
+            if(resultBox.getText().length() == 0)
+                resultBox.append("0");
             resultBox.append(".");
     }
 
@@ -41,46 +47,55 @@ public class MainActivity extends AppCompatActivity {
 
     public void button1Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("1");
     }
 
     public void button2Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("2");
     }
 
     public void button3Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("3");
     }
 
     public void button4Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("4");
     }
 
     public void button5Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("5");
     }
 
     public void button6Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("6");
     }
 
     public void button7Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("7");
     }
 
     public void button8Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("8");
     }
 
     public void button9Pressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
+        setupResultBox();
         resultBox.append("9");
     }
 
@@ -94,10 +109,11 @@ public class MainActivity extends AppCompatActivity {
                 number = 0;
             }
             resultBox.setText("");
+            operation = '+';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = '+';
     }
 
     public void minusPressed(View view) {
@@ -110,10 +126,11 @@ public class MainActivity extends AppCompatActivity {
                 number = 0;
             }
             resultBox.setText("");
+            operation = '-';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = '-';
     }
 
     public void timesPressed(View view) {
@@ -126,10 +143,11 @@ public class MainActivity extends AppCompatActivity {
                 number = 0;
             }
             resultBox.setText("");
+            operation = '*';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = '*';
     }
 
     public void dividePressed(View view) {
@@ -142,10 +160,11 @@ public class MainActivity extends AppCompatActivity {
                 number = 0;
             }
             resultBox.setText("");
+            operation = '/';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = '/';
     }
 
     public void powerPressed(View view) {
@@ -158,10 +177,11 @@ public class MainActivity extends AppCompatActivity {
                 number = 0;
             }
             resultBox.setText("");
+            operation = '^';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = '^';
     }
 
     public void rootPressed(View view) {
@@ -174,10 +194,11 @@ public class MainActivity extends AppCompatActivity {
                 number = 2;
             }
             resultBox.setText("");
+            operation = '√';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = '√';
     }
 
     public void logPressed(View view) {
@@ -190,50 +211,61 @@ public class MainActivity extends AppCompatActivity {
                 number = 10;
             }
             resultBox.setText("");
+            operation = 'l';
         } else {
             resultBox.setText(String.format(format.format(number)));
+            operation = '0';
         }
-        operation = 'l';
     }
 
     public void equalsPressed(View view) {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
-        switch(operation) {
-            case '+':
-                resultBox.setText(String.format(format.format(number + Double.parseDouble(resultBox.getText().toString()))));
-                ToggleButton plus = (ToggleButton) findViewById(R.id.toggleButtonPlus);
-                plus.setChecked(false);
-                break;
-            case '-':
-                resultBox.setText(String.format(format.format(number - Double.parseDouble(resultBox.getText().toString()))));
-                ToggleButton minus = (ToggleButton) findViewById(R.id.toggleButtonMinus);
-                minus.setChecked(false);
-                break;
-            case '*':
-                resultBox.setText(String.format(format.format(number * Double.parseDouble(resultBox.getText().toString()))));
-                ToggleButton times = (ToggleButton) findViewById(R.id.toggleButtonTimes);
-                times.setChecked(false);
-                break;
-            case '/':
-                resultBox.setText(String.format(format.format(number / Double.parseDouble(resultBox.getText().toString()))));
-                ToggleButton divide = (ToggleButton) findViewById(R.id.toggleButtonDivide);
-                divide.setChecked(false);
-                break;
-            case '^':
-                resultBox.setText(String.format(format.format(Math.pow(number, Double.parseDouble(resultBox.getText().toString())))));
-                ToggleButton power = (ToggleButton) findViewById(R.id.toggleButtonPower);
-                power.setChecked(false);
-                break;
-            case '√':
-                resultBox.setText(String.format(format.format(Math.pow(Double.parseDouble(resultBox.getText().toString()), 1 / number))));
-                ToggleButton root = (ToggleButton) findViewById(R.id.toggleButtonRoot);
-                root.setChecked(false);
-                break;
-            case 'l':
-                resultBox.setText(String.format(format.format(Math.log10(Double.parseDouble(resultBox.getText().toString())) / Math.log10(number))));
-                ToggleButton log = (ToggleButton) findViewById(R.id.toggleButtonLog);
-                log.setChecked(false);
-                break;
+        Button equals = (Button) findViewById(R.id.buttonEquals);
+        if(equals.getText().equals("AC")) {
+            resultBox.setText("");
+            equals.setText("=");
+            return;
+        }
+        if(resultBox.getText().length() > 0) {
+            switch (operation) {
+                case '+':
+                    resultBox.setText(String.format(format.format(number + Double.parseDouble(resultBox.getText().toString()))));
+                    ToggleButton plus = (ToggleButton) findViewById(R.id.toggleButtonPlus);
+                    plus.setChecked(false);
+                    break;
+                case '-':
+                    resultBox.setText(String.format(format.format(number - Double.parseDouble(resultBox.getText().toString()))));
+                    ToggleButton minus = (ToggleButton) findViewById(R.id.toggleButtonMinus);
+                    minus.setChecked(false);
+                    break;
+                case '*':
+                    resultBox.setText(String.format(format.format(number * Double.parseDouble(resultBox.getText().toString()))));
+                    ToggleButton times = (ToggleButton) findViewById(R.id.toggleButtonTimes);
+                    times.setChecked(false);
+                    break;
+                case '/':
+                    resultBox.setText(String.format(format.format(number / Double.parseDouble(resultBox.getText().toString()))));
+                    ToggleButton divide = (ToggleButton) findViewById(R.id.toggleButtonDivide);
+                    divide.setChecked(false);
+                    break;
+                case '^':
+                    resultBox.setText(String.format(format.format(Math.pow(number, Double.parseDouble(resultBox.getText().toString())))));
+                    ToggleButton power = (ToggleButton) findViewById(R.id.toggleButtonPower);
+                    power.setChecked(false);
+                    break;
+                case '√':
+                    resultBox.setText(String.format(format.format(Math.pow(Double.parseDouble(resultBox.getText().toString()), 1 / number))));
+                    ToggleButton root = (ToggleButton) findViewById(R.id.toggleButtonRoot);
+                    root.setChecked(false);
+                    break;
+                case 'l':
+                    resultBox.setText(String.format(format.format(Math.log10(Double.parseDouble(resultBox.getText().toString())) / Math.log10(number))));
+                    ToggleButton log = (ToggleButton) findViewById(R.id.toggleButtonLog);
+                    log.setChecked(false);
+                    break;
+            }
+            operation = '=';
+            equals.setText("AC");
         }
     }
 
@@ -253,7 +285,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void setupResultBox() {
         TextView resultBox = (TextView) findViewById(R.id.resultBox);
-        if((resultBox.getText().toString().length() > 0 && Double.parseDouble(resultBox.getText().toString()) == 0) || number == 0)
+        Button equals = (Button) findViewById(R.id.buttonEquals);
+        if((resultBox.getText().toString().length() > 0 && Double.parseDouble(resultBox.getText().toString()) == 0 && !resultBox.getText().toString().equals("0.")) || operation == '=') {
             resultBox.setText("");
+            operation = '0';
+            equals.setText("=");
+        }
     }
 }
